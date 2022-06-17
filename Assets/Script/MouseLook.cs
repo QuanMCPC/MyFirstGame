614bb50250e4cc787fb8399581a0981274e1ad89
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2022-06-17 22:01:01 +0700

    First and probally the last commit

---
+++ Assets/Script/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSens = 100f;
+    public Transform playerBody;
+
+    float rotationX = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime; //Side to side
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime; //Up and down
+        playerBody.Rotate(Vector3.up * mouseX);
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+    }
+}
---
+++ Assets/Script/Panel_1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel_1 : MonoBehaviour
+{
+    public GameObject panel;
+    void OnMouseDown()
+    {
+        panel.SetActive(true);
+        Time.timeScale = 0f;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
